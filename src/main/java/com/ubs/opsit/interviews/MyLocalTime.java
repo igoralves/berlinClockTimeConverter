cfd790bffa48c95e37b183d43079775e5196d382
Igor Alves <bonfarj@gmail.com>
2016-03-19 11:58:07 +0100

    Fixing MyLocalTime in order to make the tests pass.

--- src/main/java/com/ubs/opsit/interviews/MyLocalTime.java
+++ src/main/java/com/ubs/opsit/interviews/MyLocalTime.java
@@ -5,20 +5,77 @@ package com.ubs.opsit.interviews;
  */
 public class MyLocalTime {
 
+    public static final int HOUR_INDEX = 0;
+    public static final int MINUTE_INDEX = 1;
+    public static final int SECOND_INDEX = 2;
+
     private final int hour;
     private final int minute;
     private final int second;
 
-    // TODO validate data
-    // TODO create tests
+    /**
+     * Constructor
+     *
+     * @param time String with the time using HH:mm or HH:mm:ss format
+     */
     public MyLocalTime(String time) {
 
         final String[] timeArray = time.split(":");
 
-        hour = Integer.parseInt(timeArray[0]);
-        minute = Integer.parseInt(timeArray[1]);
-        second = Integer.parseInt(timeArray[2]);
+        if (timeArray.length < 2 || timeArray.length > 3) {
+            throw new IllegalArgumentException("The time format must be HH:mm or HH:mm:ss");
+        }
+
+        // set hour/minute
+        this.hour = getValue(timeArray[HOUR_INDEX]);
+        this.minute = getValue(timeArray[MINUTE_INDEX]);
+
+        // set second checking if there is second in the parameter
+        if (timeArray.length == 3) {
+            this.second = getValue(timeArray[SECOND_INDEX]);
+        } else {
+            this.second = 0;
+        }
+
+        validateTimeValues();
     }
+
+    /**
+     * Converts a String to int, throwing an unchecked exception if it is not possible
+     *
+     * @param s String to be converted
+     * @return An int
+     */
+    private int getValue(String s) {
+
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("'" + s + "' " + "is not a valid number");
+        }
+    }
+
+    /**
+     * Validate the values for hour, minute and second
+     */
+    private void validateTimeValues() {
+
+        if (hour > 24) {
+            throw new IllegalArgumentException("The hour cannot be greater than 24");
+        } else if (hour == 24 && (minute > 0 || second > 0)) {
+            throw new IllegalArgumentException("If the hour is 24, the minute and the second must be 0");
+        }
+
+        if (minute > 59) {
+            throw new IllegalArgumentException("The minute cannot be greater than 59");
+        }
+
+        if (second > 59) {
+            throw new IllegalArgumentException("The second cannot be greater than 59");
+        }
+    }
+
+    /* GETTERS */
 
     public int getHour() {
         return hour;
--- src/test/java/com/ubs/opsit/interviews/MyLocalTimeTest.java
+++ src/test/java/com/ubs/opsit/interviews/MyLocalTimeTest.java
@@ -10,48 +10,55 @@ import static org.junit.Assert.assertEquals;
 public class MyLocalTimeTest {
 
     @Test
-    public void createMyLocalTime() {
+    public void createMyLocalTimeWithSuccess() {
 
-        final MyLocalTime myLocalTime = new MyLocalTime("01:02:03");
+        // HH:mm:ss
+        final MyLocalTime myLocalTime1 = new MyLocalTime("01:02:03");
 
-        assertEquals(1, myLocalTime.getHour());
-        assertEquals(2, myLocalTime.getMinute());
-        assertEquals(3, myLocalTime.getSecond());
+        assertEquals(1, myLocalTime1.getHour());
+        assertEquals(2, myLocalTime1.getMinute());
+        assertEquals(3, myLocalTime1.getSecond());
+
+        // HH:mm
+        final MyLocalTime myLocalTime2 = new MyLocalTime("04:05");
+
+        assertEquals(4, myLocalTime2.getHour());
+        assertEquals(5, myLocalTime2.getMinute());
+        assertEquals(0, myLocalTime2.getSecond());
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidHour240001() {
         new MyLocalTime("24:00:01");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidHour240100() {
         new MyLocalTime("24:01:00");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidHour250000() {
         new MyLocalTime("25:00:00");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidMinute() {
         new MyLocalTime("00:60:00");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidSecond() {
         new MyLocalTime("00:00:60");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidCharacters() {
         new MyLocalTime("A:A:A");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = IllegalArgumentException.class)
     public void createMyLocalTimeWithInvalidSyntax() {
         new MyLocalTime("1");
     }
-
 }
